a1547ad1f54b20315f875b7cc73d47726b34c1c1
Bobby Allen <bobbyallen.uk@gmail.com>
2013-09-26 15:56:17 +0100

    Some links added to the copyright inlcude... links to the license and the project homepage basically.

--- app/views/includes/copyright.blade.php
+++ app/views/includes/copyright.blade.php
@@ -1,3 +1,3 @@
 <footer>
-    <p>&copy; Turbine Project, 2013. Turbine is written by Bobby Allen and released under the MIT license.</p>
+    <p>&copy; Turbine Project, 2013. <a href="http://bobsta63.github.io/turbine/" target="_blank">Turbine</a> is written by <a href="http://www.bobbyallen.me" target="_blank">Bobby Allen</a> and released under the <a href="http://opensource.org/licenses/MIT">MIT license</a>.</p>
 </footer>
