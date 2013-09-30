a28c4c3f94728104b3c3f4dc8d4844278fad4573
Bobby Allen <bobbyallen.uk@gmail.com>
2013-09-30 11:22:52 +0100

    Added controller code to remove a target from an existing rule set.

--- app/controllers/UtilController.php
+++ app/controllers/UtilController.php
@@ -6,6 +6,25 @@ class UtilController extends \BaseController
     public function getDeleteTarget($id, $target_hash)
     {
         // We now load in the target's nginx host file and delete the target that's name matches the md5 hash.
+        $rule = Rule::find($id);
+        if ($rule) {
+            $config = new NginxConfig();
+            $config->setHostheaders($rule->hostheader);
+            $config->readConfig(Setting::getSetting('nginxconfpath') . '/' . $config->serverNameToFileName() . '.enabled.conf');
+            $existing_targets = json_decode($config->writeConfig()->toJSON());
+            // We now iterate over each of the servers in the config file until we match the 'servers' name with the hash and when we do
+            // we delete the host from the configuration file before writing the chagnes to disk...
+            foreach ($existing_targets->nlb_servers as $target) {
+                if (md5($target->target) == $target_hash) {
+                    // Matches the target hash, we will now remove from the config file and break out the foreach.
+                    $config->removeServerFromNLB($target->target);
+                    break;
+                }
+            }
+            $config->writeConfig()->toFile(Setting::getSetting('nginxconfpath') . '/' . $config->serverNameToFileName() . '.enabled.conf');
+        }
+        return Redirect::back()
+                        ->with('flash_success', 'Target ' . strtolower(Input::get('target')) . ' has been successfully deleted from this rule!');
     }
 
     public function postAddTarget($id)
