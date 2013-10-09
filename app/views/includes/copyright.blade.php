7d85e936b734c50f6495f25050181db2f95f51cb
Bobby Allen <bobbyallen.uk@gmail.com>
2013-10-09 19:16:32 +0100

    Added HR to better improve the UX of the footer spacing and the upper page content.

--- app/views/includes/copyright.blade.php
+++ app/views/includes/copyright.blade.php
@@ -1,3 +1,4 @@
 <footer>
+    <hr>
     <p>&copy; Turbine Project, 2013. <a href="http://bobsta63.github.io/turbine/" target="_blank">Turbine</a> is written by <a href="http://www.bobbyallen.me" target="_blank">Bobby Allen</a> and released under the <a href="http://opensource.org/licenses/MIT">MIT license</a>.</p>
 </footer>
