a2008e794f9e14e416eaa343a704717ed0c9a997
Bobby Allen <bobbyallen.uk@gmail.com>
2013-10-09 18:59:45 +0100

    Removal of old OverviewController and overview view files, removed the link from the navbar include also.

--- app/views/includes/navbar.blade.php
+++ app/views/includes/navbar.blade.php
@@ -10,11 +10,6 @@
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                @if(Request::segment(1) == '')
-                <li class="active"><a href="{{ URL::route('.index') }}">Overview</a></li>
-                @else
-                <li><a href="{{ URL::route('.index') }}">Overview</a></li>
-                @endif
                 @if(Request::segment(1) == 'rules')
                 <li class="active"><a href="{{ URL::route('rules.index') }}">Sites &amp; Servers</a></li>
                 @else
