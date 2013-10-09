10aaf444b12ab01ff3f4369685d4ab5addbea6e9
ballen <ballen@172.25.87.201>
2013-10-09 18:54:54 +0100

    Removed redundent  'OverviewController' from the route file as this was not needed, replacing this with a Redirect instead.

--- app/routes.php
+++ app/routes.php
@@ -3,7 +3,13 @@
 /**
  * Standard application routes
  */
-Route::resource('/', 'OverviewController', array('only' => array('index')));
+Route::get('/', array('as' => '.index', function() {
+        // Decided to remove the redundent 'OverviewController' as there was little point of using it!
+        // Instead I am now using the Redirect::route to redirect to the rules controller.
+        // Note that I've named this route '.index' as this was the generated controller name originally.. this stops
+        // issues with the existing template rules which are trying to generate URL's from the route names..
+        return Redirect::route('rules.index');
+    }));
 Route::resource('rules', 'RulesController');
 Route::resource('settings', 'SettingsController', array('only' => array('index', 'store')));
 Route::controller('action', 'UtilController');
@@ -12,7 +18,7 @@ Route::get('logout', array('as' => 'logout', function() {
         Auth::logout();
         return Redirect::route('.index');
     }));
-    
+
 /**
  * API route grouping
  */
