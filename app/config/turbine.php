a10dc8b597d1711aaa5eaf30ce25dec187ecc73f
Bobby Allen <bobbyallen.uk@gmail.com>
2013-10-16 21:48:55 +0100

    Updated number of minutes of which to cache the update details for.

--- app/config/turbine.php
+++ app/config/turbine.php
@@ -2,5 +2,5 @@
 
 return array(
     'updateurl' => 'http://bobbyallen.me/turbine.json', // The URL for the update checker to look for updates.
-    'updatecache' => 3600, // Number of minutes to cache the updates info for, default is '3600' (1 hour)
+    'updatecache' => 60, // Number of minutes to cache the updates info for, default is '60' (1 hour)
 );
