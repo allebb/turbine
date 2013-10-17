dd3dcb2fb2f4f6cfa584537c419bea1a99b20bd6
Bobby Allen <bobbyallen.uk@gmail.com>
2013-10-17 11:54:44 +0100

    Added exception checking to the version checker, implemented inside the Cache::remember section to speed up navigation when external internet access is resticted (outbound firewall'd)/proxied etc.

--- app/controllers/SettingsController.php
+++ app/controllers/SettingsController.php
@@ -14,9 +14,9 @@ class SettingsController extends \BaseController
 
     function __construct()
     {
-        // Require that the user is logged in!
+// Require that the user is logged in!
         $this->beforeFilter('auth.basic');
-        // We want to enable CSFR protection on both the 'store' action.
+// We want to enable CSFR protection on both the 'store' action.
         $this->beforeFilter('csrf', array('on' => 'store'));
     }
 
@@ -26,11 +26,19 @@ class SettingsController extends \BaseController
     public function index()
     {
         $settings = Setting::where('usersetting', true)->orderBy('name')->get();
+
+
         // We'll make a web service request and cache the results on an hourly basis... saves bandwidth and speeds up performance!
         $update_service = Cache::remember('update_checker', Config::get('turbine.updatecache'), function() {
-                            $wsresult = HttpRequest::get(Config::get('turbine.updateurl'))->send();
-                            return json_decode(json_encode(array('last_check' => Carbon::now()->format('l jS \\of F Y \\a\\t h:i:s A'), 'result' => $wsresult->body)));
+                            try {
+                                $wsresult = HttpRequest::get(Config::get('turbine.updateurl'))->send();
+                                return json_decode(json_encode(array('last_check' => Carbon::now()->format('l jS \\of F Y \\a\\t h:i:s A'), 'result' => $wsresult->body)));
+                            } catch (Exception $e) {
+                                //dd($e->getMessage()); // Get the details of the exection!
+                                return json_decode(json_encode(array('last_check' => Carbon::now()->format('l jS \\of F Y \\a\\t h:i:s A'), 'result' => false)));
+                            }
                         });
+
 
         return View::make('settings.index')
                         ->with('title', 'Settings')
--- app/views/partials/updates.blade.php
+++ app/views/partials/updates.blade.php
@@ -1,6 +1,10 @@
-@if(Setting::getSetting('version') >= $versioninfo->result->version)
-<p class="text-success">You are currently running the latest version (<strong>{{ Setting::getSetting('version') }}</strong>) of Turbine!</p>
+@if($versioninfo->result)
+    @if(Setting::getSetting('version') >= $versioninfo->result->version)
+    <p class="text-success">You are currently running the latest version (<strong>{{ Setting::getSetting('version') }}</strong>) of Turbine!</p>
+    @else
+    <p class="text-danger">There is an update (<strong>{{ $versioninfo->result->version }}</strong>) avaliable for Turbine, you're currently running version (<strong>{{ Setting::getSetting('version') }}</strong>) we recommend that all users <a href="{{ $versioninfo->result->upgrades }}" target="_blank">upgrade</a> at their earliest convenience.</p>
+    @endif
 @else
-<p class="text-danger">There is an update (<strong>{{ $versioninfo->result->version }}</strong>) avaliable for Turbine, you're currently running version (<strong>{{ Setting::getSetting('version') }}</strong>) we recommend that all users <a href="{{ $versioninfo->result->upgrades }}" target="_blank">upgrade</a> at their earliest convenience.</p>
+<p class="text-warning">There was an error connecting to the version checker web service, please check the version manually, you're currently running version (<strong>{{ Setting::getSetting('version') }}</strong>).</p>
 @endif
 <small>Last checked for updates at {{ $versioninfo->last_check }}.</small>
